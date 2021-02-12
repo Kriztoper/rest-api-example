84ca219bbc4951fdd5ea9814547a3b4415360208
Kriztoper Urmeneta <kriztoper.urmeneta@orangeandbronze.com>
2021-02-12 13:41:31 +0800

    After posting new profile status ok is received

---
+++ readme.md
@@ -0,0 +1,12 @@
+GET all profiles = http://localhost:8080/api/profiles
+
+POST new profile = http://localhost:8080/api/profiles
+with body
+{
+    "fullName": "Jane Doe",
+    "email": "janedoe@gmail.com",
+    "mobileNumber": "09123456789",
+    "dateOfBirth": "2001-02-12",
+    "age": 20,
+    "gender": "F"
+}
--- src/main/java/io/bootify/rest_api_example/rest/ProfileController.java
+++ src/main/java/io/bootify/rest_api_example/rest/ProfileController.java
@@ -1,13 +1,23 @@
 package io.bootify.rest_api_example.rest;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.bootify.rest_api_example.model.ProfileDTO;
 import io.bootify.rest_api_example.service.ProfileService;
-import java.util.List;
-import javax.validation.Valid;
-import org.springframework.http.HttpStatus;
 
 
 @RestController
@@ -31,7 +41,7 @@ public class ProfileController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public Long createProfile(@RequestBody @Valid final ProfileDTO profileDTO) {
         return profileService.create(profileDTO);
     }
