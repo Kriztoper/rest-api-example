2e043518aae54d22ac1cc54cd51065177276cdac
Kriztoper Urmeneta <kriztoper.urmeneta@orangeandbronze.com>
2021-02-12 13:33:24 +0800

    Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO profile
+    (profile_id, full_name, email, mobile_number, date_of_birth, age, gender)
+VALUES
+    (1, 'John Doe', 'johndoe@gmail.com', '09123456789', TO_DATE('12/02/2001', 'DD/MM/YYYY'), 20, 'M');
